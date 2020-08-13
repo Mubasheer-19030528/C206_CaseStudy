54d5fe94f3d044ad72e04237a67e376c44018158
19011721 <19011721@Nemi>
2020-08-13 15:18:01 +0800

    Update

--- C206_CaseStudy/src/PurchaseOrder.java
+++ C206_CaseStudy/src/PurchaseOrder.java
@@ -12,4 +12,51 @@
  */
 public class PurchaseOrder {
 
+	private String itemName;
+	private int itemQuantity;
+	private double itemUnitPrice;
+	private String deliveryDate;
+	
+	public PurchaseOrder (String itemName,int itemQuantity,double itemUnitPrice,String deliveryDate) {
+		this.itemName = itemName;
+		this.itemQuantity = itemQuantity;
+		this.itemUnitPrice = itemUnitPrice;
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	public double getItemUnitPrice() {
+		return itemUnitPrice;
+	}
+
+	public void setItemUnitPrice(double itemUnitPrice) {
+		this.itemUnitPrice = itemUnitPrice;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+	
+	public String toString() {
+		return toString();
+	}
 }
