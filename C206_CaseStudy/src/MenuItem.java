6e52cbf211e4ead50149f9c2ca7a7c6bc7fbb546
19030528 <19030528@LAPTOP-QDI12RBC>
2020-08-13 14:41:16 +0800

    MenuItem Class Created

---
+++ C206_CaseStudy/src/MenuItem.java
@@ -0,0 +1,52 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19030528, 13 Aug 2020 2:24:28 pm
+ */
+
+public class MenuItem {
+
+	private String category;
+	private String name;
+	private double price;
+	
+	public MenuItem(String category, String name, double price) {
+		super();
+		this.category = category;
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String toString() {
+		//Write your code here
+	return toString();
+	}
+	
+}
