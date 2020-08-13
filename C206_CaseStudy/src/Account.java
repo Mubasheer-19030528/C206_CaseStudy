fb5f3cd1ed39e2f7c60a7a43a96acc31ed3a2717
19031264 <19031264@DESKTOP-5PP5MBF>
2020-08-13 14:38:37 +0800

    Account class is created

---
+++ C206_CaseStudy/src/Account.java
@@ -0,0 +1,40 @@
+
+public class Account {
+		private String username;
+		private String password;
+		private String userRole;
+		
+		public Account(String username, String password, String userRole) {
+			this.password = password;
+			this.username = username;
+			this.userRole = userRole;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getUserRole() {
+			return userRole;
+		}
+
+		public void setUserRole(String userRole) {
+			this.userRole = userRole;
+		}
+		public String toString(){
+			// Write your codes here
+		return toString();
+		}
+}
