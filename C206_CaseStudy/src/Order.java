28143746667bac2127541741d618ccf0071730a8
19011721-Daryl <68052254+19011721-Daryl@users.noreply.github.com>
2020-08-19 14:13:01 +0800

    Update Order.java
    order updated

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -5,11 +5,12 @@ public class Order {
 	private String username;
 	private String status;
 	private boolean takeaway;
-	private items ArrayList<MenuItems>;
+	private ArrayList<MenuItem> items; 
 
-	public Order(String username, String status, ) {
+	public Order(String username, String status,ArrayList<MenuItem> items ) {
 		this.username=username;
 		this.status=status;
+		this.items=items;
 		
 	}
 	public String getUsername() {
@@ -17,27 +18,31 @@ public class Order {
 	}
 	public String setUsername(String username) {
 		this.username=username;
+		return username;
 	}
 	public String getStatus() {
 		return status;
 	}
 	public String setStatus(String status) {
 		this.status=status;
+		return status;
 	}
 	public boolean isTakeaway() {
 		return takeaway;
 	}
 	public boolean setTakeaway(boolean takeaway) {
 		this.takeaway=takeaway;
+		return takeaway;
 	}
-	public ArrayList getItems() {
-		
+	public ArrayList<MenuItem> getItems() {
+		return items;
 	}
-	public void setItems(ArrayList item) {
-		
+	public void setItems(ArrayList<MenuItem> items) {
+		this.items = items;
 	}
 	public String toString(){
 		// Write your codes here
 	return toString();
 	}
 }
+
