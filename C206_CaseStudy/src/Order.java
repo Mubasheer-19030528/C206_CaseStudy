2a33fee1ab61d27f0d6872e44c1f9a3481ec9c5a
19022963 <19022963@LAPTOP-IAD9LFKF>
2020-08-19 14:05:18 +0800

    Order UPdated

---
+++ C206_CaseStudy/src/Order.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Order {
+
+	private String username;
+	private String status;
+	private boolean takeaway;
+	private items ArrayList<MenuItems>;
+
+	public Order(String username, String status, ) {
+		this.username=username;
+		this.status=status;
+		
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String setUsername(String username) {
+		this.username=username;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public String setStatus(String status) {
+		this.status=status;
+	}
+	public boolean isTakeaway() {
+		return takeaway;
+	}
+	public boolean setTakeaway(boolean takeaway) {
+		this.takeaway=takeaway;
+	}
+	public ArrayList getItems() {
+		
+	}
+	public void setItems(ArrayList item) {
+		
+	}
+	public String toString(){
+		// Write your codes here
+	return toString();
+	}
+}
